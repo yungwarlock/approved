91fe0a4709d975985b68cb997fc19ee2552aaeb8
Damian Akpan <damiakpan@gmail.com>
2024-08-23 23:51:27 +0100

    Implemented companies model. Updated project config

--- companies/models.py
+++ companies/models.py
@@ -3,9 +3,19 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
+    about = models.TextField()
+
+    date_founded = models.DateField()
+
     location = models.CharField(max_length=100)
+
+    twitter = models.URLField()
+    website = models.URLField()
     logo_url = models.URLField()
-    description = models.TextField()
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "company"
+        verbose_name_plural = "companies"
