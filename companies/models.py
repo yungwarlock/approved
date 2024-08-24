f48a6ae4a165d04fdb9700bf6809ffc475d8bfa2
Damian Akpan <damiakpan@gmail.com>
2024-08-24 08:31:07 +0100

    Implemented slug field on companies

--- companies/models.py
+++ companies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Company(models.Model):
+    slug = models.SlugField(max_length=100)
     name = models.CharField(max_length=100)
     about = models.TextField()
 
